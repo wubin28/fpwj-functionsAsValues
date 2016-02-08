0b9079ff400e041b0c792a8b470b7a270038fa66
starfish_4DAE5E1DB5 <starfish@cyber-dojo.org>
2016-02-08 13:14:29 +0000

    1

---
+++ sandbox/FunctionOverTime.java
@@ -0,0 +1,7 @@
+
+@FunctionalInterface
+public interface FunctionOverTime {
+
+    double valueAt(int time);
+
+}
---
+++ sandbox/ProfitFPTest.java
@@ -0,0 +1,34 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class ProfitFPTest {
+    @Test
+    public void should_calculate_total_profits_for_the_year_in_FP() {
+        // given
+        final double[] EXPECTED_SALES_JAN_TO_DEC =
+            new double[] { 42.0, 45.6, 43.6, 50.2, 55.6, 54.7,
+                    58.0, 57.3, 62.0, 60.3, 71.2, 88.8};
+        final FunctionOverTime sales =
+                (time) -> EXPECTED_SALES_JAN_TO_DEC[time - 1];
+
+        final FunctionOverTime fixedCosts =
+                (time) -> 15.0;
+
+        final FunctionOverTime incrementalCosts =
+                (time) -> 5.1 + 0.15 * time;
+
+        final FunctionOverTime profit =
+                (time) -> sales.valueAt(time) -
+                (fixedCosts.valueAt(time) +
+                        incrementalCosts.valueAt(time));
+
+        // when
+        Double totalProfit = 0.0;
+        for(int time = 1; time <= 12; time ++) {
+            totalProfit += profit.valueAt(time);
+        }
+
+        // then
+        assertEquals(436.4, totalProfits, 0.001);
+    }
+}
